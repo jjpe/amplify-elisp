e7ee2cafc25c0c49676455dc14b1d3c200f93f96
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-07-08 10:56:36 +0200

    Upgrade to amplify 0.14.0

--- amplify-elisp.el
+++ amplify-elisp.el
@@ -24,7 +24,7 @@ explicitly included."
 
 
 
-(defvar amplify-elisp/current-version "0.13.5"
+(defvar amplify-elisp/current-version "0.14.0"
   "The current semantic version of the Amplify Emacs module.")
 
 (defvar amplify-elisp/detected-os
